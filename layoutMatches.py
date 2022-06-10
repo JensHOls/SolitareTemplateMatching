c0e13583ce92cac81c7e7460f2265d8f4b11567b
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-06-10 11:07:16 +0200

    moved file to layoutMatches.py

--- layoutMatches.py
+++ layoutMatches.py
@@ -1,17 +1,97 @@
 import cv2
 
-def cards():
-    card_width = 57
-    card_height = 87
-return
+# collecting all matches into 'card dictionaries' with coordinates with both rank and suit
 
-def talon ():
+def divideIntoColumns(allMatches, backsideMatches):
+    allMatchesPaired = []
+    for matchOne in allMatches:
+        suitTopLeft = matchOne['topLeft']
+        suitName = matchOne['name']
+        if suitName == 'heart' or suitName == 'diamond' or suitName == 'club' or suitName == 'spade':
+            for matchTwo in allMatches:
+                typeTopLeft = matchTwo['topLeft']
+                typeName = matchTwo['name']
+                if typeName != 'heart' and typeName != 'diamond' and typeName != 'club' and typeName != 'spade' and typeName != 'backside':
+                    if suitTopLeft[1] - typeTopLeft[1] < 60:
+                        if (suitTopLeft[0] - typeTopLeft[0] < 25 and suitTopLeft[0] - typeTopLeft[0] > -25):
+                            suitRankPair = {'suitTopLeft': suitTopLeft, 'typeTopLeft': typeTopLeft,
+                                            'name': suitName + typeName}
+                            allMatchesPaired.append(suitRankPair)
+    # match seperation into column, foundation and talon cards
+    foundationMatches = []
+    columnMatches = []
+    talonMatches = []
+    talonfoundationafgraensning = (1209L, 570L)
 
-return
+    for match in allMatchesPaired:
+        if match['suitTopLeft'][0] > talonfoundationafgraensning[0] and match['suitTopLeft'][1] < \
+                talonfoundationafgraensning[1]:
+            foundationMatches.append(match)
+        if match['suitTopLeft'][0] < talonfoundationafgraensning[0] and match['suitTopLeft'][1] < \
+                talonfoundationafgraensning[1]:
+            talonMatches.append(match)
+        if match['suitTopLeft'][1] > talonfoundationafgraensning[1]:
+            columnMatches.append(match)
 
-def foundations():
-return
+    # finally column rows into columns
 
+    # we sort the cards in terms of x axis (basically, we start at the left most card(
+    columnMatches = sorted(columnMatches, key=lambda match: match['suitTopLeft'][0])
+    backsideMatches = sorted(backsideMatches, key=lambda match: match['topLeft'][0])
 
-def collums ():
-return
+    # list with 7 lists in order to seperate column
+    columnMatchesRows = [[], [], [], [], [], []]
+    backsideMatchesRows = [[], [], [], [], [], []]
+
+    index = 0
+    prev_x = 0
+    # backside into columns
+    for match in backsideMatches:
+        current_x = match['topLeft'][0]
+        difference = current_x - prev_x
+        # the first value
+        if prev_x == 0:
+            prev_x = current_x
+            backsideMatchesRows[index].append(match)
+            continue
+        # for same row
+        if difference < 10:
+            backsideMatchesRows[index].append(match)
+        # with great difference, change row index to append to
+        if difference > 10:
+            index = index + 1
+            backsideMatchesRows[index].append(match)
+        prev_x = current_x
+
+    index = 0
+    prev_x = 0
+
+    # cards into columns
+    for match in columnMatches:
+        current_x = match['suitTopLeft'][0]
+        difference = current_x - prev_x
+        # the first value
+        if prev_x == 0:
+            prev_x = current_x
+            columnMatchesRows[index].append(match)
+            continue
+        # for same row
+        if difference < 10:
+            columnMatchesRows[index].append(match)
+        # for jumping to right side
+        if difference < 227 and current_x - prev_x > 187:
+            columnMatchesRows[index].append(match)
+        # for new row
+        if difference > 10:
+            if difference > 237 or difference < 187:
+                index = index + 1
+                columnMatchesRows[index].append(match)
+        prev_x = current_x
+
+    print (backsideMatches, )
+    # now we combine the two lists of lists
+    combinedList = []
+    return combinedList
+
+# remove duplicates and false positives now (out of scope for this branch)
+# --------------------------------------------------------------------------
